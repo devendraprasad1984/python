646cdd68abace11d311e9182f4c7664defb3f04a
Devendra Prasad <devendraprasad1984@gmail.com>
2021-01-17 18:53:02 +0530

    updates

---
+++ djangoTest/ReactDjango/frontendapp/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Login from "./components/login";
+
+function App() {
+    return (
+        <div className="App">
+            <h1>Login Page django Practices</h1>
+            <Login/>
+        </div>
+    );
+}
+
+export default App;
---
+++ djangoTest/ReactDjango/frontendapp/src/components/login.js
@@ -0,0 +1,27 @@
+import React, {useRef} from 'react';
+
+export default function Login(props) {
+    // const [cred, setCred]=useState({username:'',password:''});
+    const usernameRef = useRef();
+    const passwordRef = useRef();
+
+    const handleLogin = event => {
+        const username = usernameRef.current.value;
+        const password = passwordRef.current.value;
+        const payload = {
+            method: 'POST'
+            , headers: {'Content-Type': 'application/json'}
+            , body: JSON.stringify({username, password})
+        }
+        fetch('http://127.0.0.1:8000/auth/', payload)
+            .then(data => console.log(data))
+            .catch(err => console.log(err))
+    }
+
+    return <div className="App">
+        <h1>Login Page</h1>
+        <input ref={usernameRef} type='text' placeholder='usernanme'/>
+        <input ref={passwordRef} type='password' placeholder='password'/>
+        <button onClick={handleLogin}>Login</button>
+    </div>;
+}
