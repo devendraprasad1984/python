f6a224fbfa63dab9990177cebed9e46457cf57f1
Devendra Prasad <devendraprasad1984@gmail.com>
2021-01-18 19:14:11 +0530

    updates

---
+++ ReactDjango/api/todoView.py
@@ -0,0 +1,49 @@
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from . import models
+from . import serializers
+
+@api_view(['GET'])
+def apiOverview(req):
+    api_urls = {
+        'List': '/tast-list/',
+        'Detail View': '/task-detail/<str:pk>/',
+        'Create': '/task-create/',
+        'Update': '/task-update/<str:pk>/',
+        'Delete': '/task-delete/<str:pk>/',
+    }
+    return Response(api_urls)
+
+
+@api_view(['GET'])
+def taskList(req):
+    tasks = models.Task.objects.all().order_by('-id')
+    ser = serializers.TaskSerializer(tasks, many=True)
+    return Response(ser.data)
+
+@api_view(['GET'])
+def taskDetail(req,pk):
+    tasks=models.Task.objects.get(id=pk)
+    ser=serializers.TaskSerializer(tasks, many=False)
+    return Response(ser.data)
+
+@api_view(['POST'])
+def taskCreate(req):
+    ser=serializers.TaskSerializer(data=req.data)
+    if ser.is_valid():
+        ser.save()
+    return Response(ser.data)
+
+@api_view(['POST'])
+def taskUpdate(req, pk):
+    task=models.Task.objects.get(id=pk)
+    ser=serializers.TaskSerializer(instance=task, data=req.data)
+    if ser.is_valid():
+        ser.save()
+    return Response(ser.data)
+
+@api_view(['DELETE'])
+def taskDelete(req, pk):
+    task=models.Task.objects.get(id=pk)
+    task.delete()
+    return Response('Item deleted successfully')
--- djangoTest/ReactDjango/api/models.py
+++
@@ -1,5 +0,0 @@
-from django.db import models
-
-class Books(models.Model):
-    title=models.TextField(max_length=100, null=False, blank=False)
-
