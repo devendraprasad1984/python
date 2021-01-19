6267d0b10965768af40adc6908312d2b98e4f3a7
Devendra Prasad <devendraprasad1984@gmail.com>
2021-01-19 17:54:25 +0530

    updates

--- ReactDjango/website/webroutes.py
+++ ReactDjango/website/webroutes.py
@@ -1,11 +1,13 @@
 from . import views
 from django.views.generic.base import TemplateView
 from django.contrib.auth import views as auth
-routes=[
-    (r'home/',views.home,'home'),
-    (r'about/',views.about,'about'),
-    (r'contact/',views.contact,'contact'),
-    (r'homeX/',TemplateView.as_view(template_name='index.html'),'homex'),
-    #for login auth via django pre apps
-    (r'accounts/login',auth.LoginView.as_view(template_name="accounts/login.html"),'login'),
+
+routes = [
+    (r'home/', views.home, 'home'),
+    (r'about/', views.about, 'about'),
+    (r'contact/', views.contact, 'contact'),
+    (r'homeX/', TemplateView.as_view(template_name='index.html'), 'homex'),
+    # for login auth via django pre apps
+    (r'accounts/login', auth.LoginView.as_view(template_name="accounts/login.html"), 'login'),
+    (r'accounts/logout', auth.LogoutView.as_view(template_name="home.html"), 'logout'),
 ]
