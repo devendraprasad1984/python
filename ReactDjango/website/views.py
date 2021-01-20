4c43801c7dd9afc56dddc4ec082b7bf11b8177f1
Devendra Prasad <devendraprasad1984@gmail.com>
2021-01-20 20:33:01 +0530

    updates

--- ReactDjango/website/views.py
+++ ReactDjango/website/views.py
@@ -1,6 +1,7 @@
 # from django.http import HttpResponse
 # from django.template import loader
 from django.shortcuts import render
+from django.http import HttpResponse, HttpRequest
 from django.views.generic.base import TemplateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 
@@ -9,14 +10,14 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 #     return HttpResponse(template.render({},req))
 #     return HttpResponse('Hello Welcome to Django website modules! fine lets go ahead.')
 
-def home(request):
+def home(request: HttpRequest)->HttpResponse:
     print('loggedin user', request.user)
     return render(request,'home.html',{})
 
-def about(request):
+def about(request: HttpRequest)->HttpResponse:
     return render(request,'about.html',{})
 
-def contact(request):
+def contact(request: HttpRequest)->HttpResponse:
     return render(request,'contactus.html',{})
 
 class ProfileView(LoginRequiredMixin, TemplateView):
