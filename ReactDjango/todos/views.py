7d27a99a1a80ab5094cb06b198c2815dd227388f
Devendra Prasad <devendraprasad1984@gmail.com>
2021-01-29 21:37:45 +0530

    updates

--- ReactDjango/todos/views.py
+++ ReactDjango/todos/views.py
@@ -1,23 +1,46 @@
 from django.http import HttpResponse as res, HttpRequest
 from django.shortcuts import render, redirect
 from . import models
+from django.core import serializers
+import json
+
+# from django.forms.models import model_to_dict
+
 
 redirectUrlMainPage = '/todos/view/'
 todoDB = 'todos'
+jsonType = 'application/json'
+
+
+def serialise(data, flds):
+    objx = serializers.serialize('json', data, fields=flds)
+    return objx
+
+
+def simple_serialise(data, flds):
+    jsonObj = json.loads(serializers.serialize('json', data, fields=flds))
+    for d in jsonObj:
+        del d['pk']
+        del d['model']
+    return json.dumps(jsonObj)
 
 
 def getTodoList():
-    tododata=models.Todo.objects.using(todoDB).all()
+    tododata = models.Todo.objects.using(todoDB).all()
     return tododata
 
 
 def todo_list_view(req):
-    return render(req, 'todos/index.html', {'todo_list':getTodoList()})
+    return render(req, 'todos/index.html', {'todo_list': getTodoList()})
 
 
 def todo_list(req):
-    list = getTodoList()
-    return res(list)
+    flds = ['content']
+    # obj = serialise(getTodoList(), flds)
+    obj1 = simple_serialise(getTodoList(), flds)
+    # listJson = {'data': obj}
+    return res(obj1, content_type=jsonType)
+    # return json.dumps(obj)
 
 
 def insert_todo_item(req: HttpRequest):
@@ -27,7 +50,7 @@ def insert_todo_item(req: HttpRequest):
     return redirect(redirectUrlMainPage)
 
 
-def delete_todo_item(req,todo_id):
+def delete_todo_item(req, todo_id):
     obj = models.Todo.objects.using(todoDB).get(id=todo_id)
     obj.delete()
     return redirect(redirectUrlMainPage)
