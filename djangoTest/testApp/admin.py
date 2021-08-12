21cc3b598b241adbf8c11281b137337e07ff901a
Devendra Prasad <devendraprasad1984@gmail.com>
2021-08-12 13:10:39 +0530

    update

--- djangoTest/testApp/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import movie,songs
-
-# Register your models here.
-admin.site.register(movie)
-admin.site.register(songs)
--- djangoTest/testApp/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import url
-from . import views
-
-app_name='testApp'
-urlpatterns = [
-    url(r'^$', views.index,name='details'),
-    url(r'^(?P<movie_id>[0-9]+)/$', views.getDetailsOfMovie, name='getDetails'),
-    url(r'^favourite/$', views.favourite, name='favourite'),
-]
