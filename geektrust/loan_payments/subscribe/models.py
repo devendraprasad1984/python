7a32f85adc137f8146c09072ad74781118e1c267
Devendra Prasad <devendraprasad1984@gmail.com>
2021-08-14 00:21:15 +0530

    update

--- geektrust/loan_payments/subscribe/models.py
+++ geektrust/loan_payments/subscribe/models.py
@@ -6,5 +6,5 @@ class SUBSCRIPTION(models.Model):
     id = models.AutoField(auto_created=True, primary_key=True)
     email = models.EmailField(null=False, unique=True)
     secret_key = models.TextField(null=True)
+    signer = models.TextField(null=True)
     when = models.DateTimeField(auto_now_add=True, blank=True)
-
