b2aa4b9e4776835e02b773c8623631f1d5f93956
Devendra Prasad <devendraprasad1984@gmail.com>
2021-08-13 23:57:00 +0530

    update

--- geektrust/loan_payments/subscribe/views.py
+++ geektrust/loan_payments/subscribe/views.py
@@ -1,13 +1,16 @@
 from django.shortcuts import render, HttpResponse as res
 from django.http import HttpRequest
 from loan_manager.common import config
+from django.views.decorators.csrf import csrf_exempt
 
 
 # Create your views here.
 def fn_GET_NEW_TOKEN(req: HttpRequest):
+    if req.method == config.GET:
+        return res(config.NO_OP_ALLOWED)
     return res('here is your new token')
 
-
+@csrf_exempt
 def fn_SUBSCRIBE(req: HttpRequest):
     if req.method == config.GET:
         return res(config.NO_OP_ALLOWED)
