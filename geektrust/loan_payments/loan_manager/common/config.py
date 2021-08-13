12dd76ef482fde61c163de218ae9ca7c7e796657
Devendra Prasad <devendraprasad1984@gmail.com>
2021-08-14 00:20:40 +0530

    update

--- geektrust/loan_payments/loan_manager/common/config.py
+++ geektrust/loan_payments/loan_manager/common/config.py
@@ -3,20 +3,24 @@ from uuid import uuid4
 import base64
 from django.core import serializers
 from .. import models
+from django.utils import crypto
+from django.core.signing import Signer
 
 failed = "failed"
 success = "success"
 GET = 'GET'
 POST = 'POST'
 header = 'HEADER'
+app_code = 'g3eK_t7R_#278_s___T'
 len_of_uid = 17
+X_GEEK_HEADER = 'x-geek-trust-key'
 CONTENT_TYPE = "application/json"
 NO_OP_ALLOWED = json.dumps({"msg": "operation not allowed", "status": failed})
 
 
 def getJsonSet(qset):
     data = json.loads(serializers.serialize('json', qset))
-    rows=[f['fields'] for f in data]
+    rows = [f['fields'] for f in data]
     return rows
 
 
@@ -27,6 +31,20 @@ def getBodyFromReq(req):
 def getuuid():
     uuid = base64.urlsafe_b64encode(uuid4().bytes).rstrip(b'=').decode('ascii')
     return uuid[0: len_of_uid].__str__()
+
+
+def getSecretAccessKey():
+    return crypto.get_random_string(len_of_uid)
+
+
+def getSignerObject():
+    signer = Signer()
+    return signer.sign_object({'key': getSecretAccessKey(), 'app_code': app_code})
+
+
+def getUnsigned(signkey):
+    signer = Signer()
+    return signer.unsign_object(signkey)
 
 
 def get_uniq_bankid():
@@ -46,7 +64,7 @@ def get_uniq_loanid():
 
 def addlog(type, logObj):
     try:
-        dblog=models.QUERY_LOG(
+        dblog = models.QUERY_LOG(
             type=type,
             log=json.dumps(logObj)
         )
--- geektrust/loan_payments/subscribe/views.py
+++ geektrust/loan_payments/subscribe/views.py
@@ -2,20 +2,33 @@ from django.shortcuts import render, HttpResponse as res
 from django.http import HttpRequest
 from loan_manager.common import config
 from django.views.decorators.csrf import csrf_exempt
+import json
 
 
 # Create your views here.
 def fn_GET_NEW_TOKEN(req: HttpRequest):
-    if req.method == config.GET:
+    if req.method == config.POST:
         return res(config.NO_OP_ALLOWED)
-    return res('here is your new token')
+    key = config.getSecretAccessKey()
+    sign = config.getSignerObject()
+    output = {"key": key, "signed": sign, "msg": "here is your secret key, keep it safe", "status": config.success}
+    return res(json.dumps(output), content_type=config.CONTENT_TYPE)
+
 
 @csrf_exempt
 def fn_SUBSCRIBE(req: HttpRequest):
     if req.method == config.GET:
         return res(config.NO_OP_ALLOWED)
-
-    return res('subscription done. you secret key has been mailed')
+    body = config.getBodyFromReq(req)
+    name = body['name']
+    email = body['email']
+    key = config.getSecretAccessKey()
+    sign = config.getSignerObject()
+    output = {"key": key, "signed": sign,
+              "msg": f'thanks {name}! for subscribing our apis and saas solutions. you secret key has been mailed to you'
+                     f' at {email}. use it in header {config.X_GEEK_HEADER} for accessing our services. this will be valid for next 1 yeae, '
+                     f'you have to get it regnerated for further use', "status": config.success}
+    return res(json.dumps(output), content_type=config.CONTENT_TYPE)
 
 
 def gn_GET_SUBSCRIBERS(req: HttpRequest):
