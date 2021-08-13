a21f0098d7545663047c2fb8b405d193952d351d
Devendra Prasad <devendraprasad1984@gmail.com>
2021-08-13 11:04:12 +0530

    updates

---
+++ geektrust/loan_payments/bank_manager/validations/validate.py
@@ -0,0 +1,20 @@
+from ..models import BANKS
+
+
+def validate_input_add_new_bank(inputs):
+    flag = True
+    msg = 'all good'
+    name = inputs['name']
+    if name == '' or len(name) > 10:
+        msg = 'name is blank or length is more than 10 characters'
+        flag = False
+    else:
+        try:
+            found = BANKS.objects.get(name=name)
+        except BANKS.DoesNotExist:
+            found = None
+        if found != None and found.id != None:
+            msg = 'record already exists'
+            flag = False
+
+    return {"status": flag, "msg": msg}
--- geektrust/loan_payments/bank_manager/views.py
+++ geektrust/loan_payments/bank_manager/views.py
@@ -4,6 +4,7 @@ from django.views.decorators.csrf import csrf_exempt
 from loan_manager.common import config
 import json
 from bank_manager import models
+from .validations import validate as bankValidations
 
 
 # Create your views here.
@@ -13,24 +14,31 @@ def fn_ADD_BANK(req: HttpRequest):
         return res(config.NO_OP_ALLOWED)
     body = config.getBodyFromReq(req)
     name = body['name']
-    uid = config.get_uniq_bankid()
-    success = {}
-    failed = {}
+    inputs = {"name": name}
     flag = True
-    try:
-        model = models.BANKS(name=name, uid=uid)
-        model.save()
-        success = {
-            "msg": f'bank {name} added - {uid}',
-            "status": config.success
-        }
-    except Exception as ex:
+    validate = bankValidations.validate_input_add_new_bank(inputs)
+    if validate['status'] == True:
+        try:
+            uid = config.get_uniq_bankid()
+            # model = models.BANKS(name=name, uid=uid)
+            # model.save()
+            success = {
+                "msg": f'bank {name} added - {uid}',
+                "status": config.success
+            }
+        except Exception as ex:
+            failed = {
+                "msg": f'bank {name} not added',
+                "detail": str(ex),
+                "status": config.failed
+            }
+            flag = False
+    else:
+        flag = False
         failed = {
-            "msg": f'bank {name} not added',
-            "detail": str(ex),
+            "msg": f'{validate["msg"]}',
             "status": config.failed
         }
-        flag = False
 
     output = success if flag == True else failed
     return res(json.dumps(output), content_type=config.CONTENT_TYPE)
