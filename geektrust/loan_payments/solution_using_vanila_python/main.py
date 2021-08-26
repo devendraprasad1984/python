2e728827b24eb31275a4b28bccf65e3130f8996f
Devendra Prasad <devendraprasad1984@gmail.com>
2021-08-26 13:18:58 +0530

    update

---
+++ geektrust/loan_payments/solution_using_vanila_python/main.py
@@ -0,0 +1,11 @@
+import sys
+
+# this solution will work in memory and wont be saved
+def main():
+    input_file = sys.argv[1]
+    # sys.argv[1] should give the absolute path to the input file
+    # parse the file and process the command
+    # print the output
+
+if __name__ == "__main__":
+    main()
